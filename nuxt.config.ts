6c9b403069607d6aea885146d83f28cd1584077d
p@ppu <skpappu34563456@gmail.com>
2025-01-22 11:15:46 +0600

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import Aura from '@primevue/themes/aura';
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -13,11 +15,7 @@ export default defineNuxtConfig({
       theme: {
           preset: Aura
       }
+    }
   }
-}
-
 })
 
-function defineNuxtConfig(arg0: { compatibilityDate: string; devtools: { enabled: boolean; }; modules: string[]; primevue: { options: { theme: { preset: any; }; }; }; }) {
-  throw new Error('Function not implemented.');
-}
